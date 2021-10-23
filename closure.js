5587e007457c68fa0ab2f22cc7886ea0ddf323b9
adityahazarika <46670592+adityahazarika@users.noreply.github.com>
2021-10-23 13:42:33 +0530

    Update closure.js

--- closure.js
+++ closure.js
@@ -45,3 +45,20 @@ alert(dwightSalary.currentAmount()); // $65,000
 dwightSalary.lower();
 dwightSalary.lower();
 alert(dwightSalary.currentAmount()); // $55,000
+
+
+
+
+// example
+function a(num){
+    let x = 1;
+    return function b(){
+        let y = 2;
+        return function fun1(){
+            let str = "Namaste";
+            console.log(x,y,c,num,str)
+        }
+    }
+}
+var c = 3;
+a(8)()();
