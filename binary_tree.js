081b3c7da3252001fffaf692cce9c9e1e12d2d7b
adityahazarika <46670592+adityahazarika@users.noreply.github.com>
2023-04-16 13:21:02 +0530

    Create binary_tree.js

---
+++ binary_tree.js
@@ -0,0 +1,39 @@
+let arr = [3,5,6,9,12,465,999];
+
+function fun1(arr,n){
+    if(arr.length==1){
+        if(n==arr[0]){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    if(arr.length==2){
+        if(arr[0]==n){
+            return true
+        }
+        else if(arr[1]==n){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else{
+        let len = Math.floor(arr.length/2);
+        if(n==arr[len-1]){
+            return true
+        }
+        else if(n<arr[len-1]){
+            arr.length = len;
+            return fun1(arr,n)
+        }
+        else{
+            arr.splice(0,len);
+            return fun1(arr,n)
+        }
+    }
+}
+
+console.log(fun1(arr,3))
