496223aa86dcb7edf6f8412819227987d484ffe4
adityahazarika <46670592+adityahazarika@users.noreply.github.com>
2023-06-12 15:10:18 +0530

    Update interview_questions.js

--- interview_questions.js
+++ interview_questions.js
@@ -26,3 +26,19 @@ var x = 2;
     var x = 3;
   })();
 })();
+
+
+Q5
+function changeStuff(a,b,c){
+    a = a*10;
+    b.item = "changed";
+    c = {item : "changed"};
+}
+
+var num = 10;
+var obj1 = {item:"unchanged"};
+var obj2 = {item:"unchanged"};
+
+changeStuff(num,obj1,obj2);
+
+console.log(num, obj1, obj2)
