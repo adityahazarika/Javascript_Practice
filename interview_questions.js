5c671fdef0f053ce0da6a071397e1bc965d0e344
adityahazarika <46670592+adityahazarika@users.noreply.github.com>
2023-06-13 14:45:36 +0530

    Update interview_questions.js

--- interview_questions.js
+++ interview_questions.js
@@ -42,3 +42,28 @@ var obj2 = {item:"unchanged"};
 changeStuff(num,obj1,obj2);
 
 console.log(num, obj1, obj2)
+
+Q6
+// Following is the price arr of a product ordered in day wise, we need to find the highest profit we could have booked if we bought it at least price and sell it at high price.
+// For example in this array if we have bought it at price of 2 and sold it at price of 10 we could have booked highest profit.
+var arr = [11,2,4,6,7,3,10,1,0];
+
+let a = arr[0];
+let obj = {
+    profit: 0,
+    ind1:0,
+    ind2:0
+}
+
+for(let i=0;i<arr.length;i++){
+    if(arr[i]<a){
+        a = arr[i];
+    }
+    else if((arr[i]-a)>obj.profit){
+        obj.profit = arr[i]-a;
+        obj.ind1 = a;
+        obj.ind2 = arr[i]
+    }
+}
+
+console.log(obj)
